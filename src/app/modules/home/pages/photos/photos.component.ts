181170f4a5454ff703ed4f38a38596a11016f5f9
janik-krzysztof <krzysztof.janik@onwelo.com>
2021-09-19 16:13:29 +0200

    Zdjęcia

--- src/app/modules/home/pages/photos/photos.component.ts
+++ src/app/modules/home/pages/photos/photos.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.scss']
 })
-export class PhotosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PhotosComponent {
+  photosUrl: string = 'https://drive.google.com/drive/folders/1Yr4HvxNd1gHnTLga5EjbjLa3iZzJTP-7?usp=sharing';
 }
